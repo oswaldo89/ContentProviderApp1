2032d00a14fd6186110ff2df0ab5aac124fb77b8
Oswaldo Gomez <oswaldo.gh89@gmail.com>
2017-05-05 13:09:27 -0500

    primer comit

---
+++ app/src/main/java/com/oswaldogh89/app1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.oswaldogh89.app1;
+
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ContentValues values = new ContentValues();
+        values.put(MyProvider.users, "oswaldo.gomez@bsdap.com");
+        getContentResolver().insert(MyProvider.CONTENT_URI, values);
+
+
+        Toast.makeText(getBaseContext(), "New record inserted", Toast.LENGTH_LONG).show();
+    }
+}
